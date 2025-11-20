ff3b272b88498c2c18faf8a5cce9849753414e51
tajbaba999 <tajbaba153@gmail.com>
2025-11-20 09:47:20 +0530

    update the ingest function

--- inngest/functions.ts
+++ inngest/functions.ts
@@ -1,13 +1,11 @@
 import { inngest } from "./client";
 
-
 export const helloWorld = inngest.createFunction(
-    {id : "hello-world"},
-    {event: "vibe/hello-world"},
-    async ({ event,  step }) => {
-     await step.run("hello-world", async() =>{
-        return { message: `Hello ${event.data.name}!` };
-    });
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+    await step.sleep("wait-a-moment", "1s");
+    return { message: `Hello ${event.data.email || "World"}!` };
   },
 );
 
